ba4ed4d42863274bba9dad5040c0ce1d7705d6f1
WhiskeyJack <b.santema@inthere.nl>
2012-11-01 12:15:18 +0000

    Games can now have a youtube video.

--- portal_games/includes/js/portal_games.js
+++ portal_games/includes/js/portal_games.js
@@ -1,6 +1,7 @@
 (function ($) {
   $(document).ready(function(){
-    var gameurl = Drupal.settings.param.gameurl;    
+    var gameurl = Drupal.settings.param.gameurl;
+    var videourl = Drupal.settings.param.videourl;
     var closebtn = Drupal.settings.param.closebtn == 1 ? true : false;
     $(".game_play_btn").click(function() {
       $.fancybox.open({
@@ -14,6 +15,21 @@
         helpers : {
           overlay : {
             closeClick: false
+          }
+        }
+      });
+    });
+    $(".game_video_btn").click(function() {
+      $.fancybox.open({
+        href : videourl,
+        type : 'swf',
+        scrolling: 'no',
+        preload: true,
+        closeBtn: true,
+        padding: 0,
+        helpers : {
+          overlay : {
+            media: {}
           }
         }
       });
---
+++ portal_games/includes/js/youtube_modal.js
@@ -0,0 +1,13 @@
+(function ($) {
+  $(document).ready(function(){
+    $(".fancybox").fancybox({
+      openEffect	: 'none',
+      closeEffect	: 'none',
+      padding: 0,
+      helpers: {
+        media: {}
+      }
+    });
+  });
+
+})(jQuery);
